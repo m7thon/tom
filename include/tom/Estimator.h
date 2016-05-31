afc5125e00ef1e7dcafe9469e986db8090b321d4
Michael Thon <m7thon@users.noreply.github.com>
2016-05-31 17:58:34 +0200

    Fix variance estimation regularization.
    This is now controlled by three parameters, allowing various sensible versions:
    
    * `nPC` : pseudo-counts to add to probability estimates before further variance computation
    * `nZ` : compute confidence intervals for probability estimates and use "worst-case" values for variance estimation
    * `vMin` : used to set a lower bound for the variance estimation, either absolute, or related to the available data size.

--- include/tom/Estimator.h
+++ include/tom/Estimator.h
@@ -227,8 +227,7 @@ public:
 
     //<editor-fold desc="Regularization parameters">
     SWIGCODE(%feature ("kwargs") regularization;)
-    SWIGCODE(%apply double *OUTPUT { double *zConfidenceIntervalSizeOut, double *minimumVarianceOut };)
-    SWIGCODE(%apply std::string *OUTPUT { std::string *confidenceIntervalTypeOut };)
+    SWIGCODE(%apply double *OUTPUT { double *vPCOut, double *vZOut, double *vMinOut };)
     /**
      Set (optional) and then return the regularization parameters for the `Estimator`. This is done as follows:
      - First, if a `preset` ("none" / "default") is specified, all regularization parameters are set accordingly.
@@ -244,57 +243,54 @@ public:
      \else
      in the corresponding output arguments `...Out` (if not NULL).
      \endif
+
+     @param vPC number of pseudo-counts to add before computing the variance
+     @param vZ the size of the confidence interval used in the variance computation, measured by z-score
+     @param vMin determines a lower bound for the returned variance, which will simply be `vMin` if this is < 0.25, or computed as \f$vMin / (\pi N)^2\f$, where `N` is the length of the sequence data.
+     @param preset a preset that may be specified as explained above { "default", "none" }
      */
     C1(void) PY1(tuple)
-    regularization(C4(double *zConfidenceIntervalSizeOut, std::string *confidenceIntervalTypeOut, double *minimumVarianceOut,)
-                   double zConfidenceIntervalSize = -1,
-                   std::string confidenceIntervalType = "",
-                   double minimumVariance = -1,
+    regularization(C4(double *vPCOut, double *vZOut, double *vMinOut,)
+                   double vPC = -1, double vZ = -1, double vMin = -1,
                    std::string preset = "") throw(std::invalid_argument) {
+        // Set to preset
         if (preset == "default") {
-            z = 1;
-            confidenceIntervalType_ = "Wilson";
-            minimumVariance_         = 0;
+            vPC_ = 0;
+            vZ_ = 1;
+            vMin_ = 5;
         } else if (preset == "none") {
-            z = 0;
-            confidenceIntervalType_ = "Wilson";
-            minimumVariance_         = 0;
+            vPC_ = 1;
+            vZ_ = 0;
+            vMin_ = 0;
         } else if (preset != "") {
             throw std::invalid_argument("unrecognized preset");
         }
-        if (zConfidenceIntervalSize != -1) this->z = zConfidenceIntervalSize;
-        if (confidenceIntervalType != "") this->confidenceIntervalType_ = confidenceIntervalType;
-        if (minimumVariance         != -1) this->minimumVariance_ = minimumVariance;
-        if (zConfidenceIntervalSizeOut) *zConfidenceIntervalSizeOut = this->z;
-        if (confidenceIntervalTypeOut) *confidenceIntervalTypeOut = this->confidenceIntervalType_;
-        if (minimumVarianceOut) *minimumVarianceOut = this->minimumVariance_;
+        // Set individual parameters
+        if (vPC != -1) this->vPC_ = vPC;
+        if (vZ != -1) this->vZ_ = vZ;
+        if (vMin != -1) this->vMin_ = vMin;
+        // Return current parameters
+        if (vPCOut) *vPCOut = this->vPC_;
+        if (vZOut) *vZOut = this->vZ_;
+        if (vMinOut) *vMinOut = this->vMin_;
     }
-
-    SWIGCODE(%clear double *zConfidenceIntervalSizeOut, std::string *confidenceIntervalTypeOut, double *minimumVarianceOut;)
+    SWIGCODE(%clear double *vPCOut, double *vZOut, double *vMinOut;)
 
 #ifndef SWIG
 #ifndef PY
     /**
-     * Set the regularization parameters for the `Estimator`. This is a convenience function that simply calls `regularization(NULL, NULL, NULL, zConfidenceIntervalSize, confidenceIntervalType, minimumVariance, preset);` */
-    void regularization(double zConfidenceIntervalSize = -1,
-                        std::string confidenceIntervalType = "",
-                        double minimumVariance = -1,
+     * Set the regularization parameters for the `Estimator`. This is a convenience function that simply calls `regularization(NULL, NULL, NULL, zPC, vZ, vMin, preset);` */
+    void regularization(double vPC = -1, double vZ = -1, double vMin = -1,
                         std::string preset = "") throw(std::invalid_argument) {
-        regularization(NULL, NULL, NULL, zConfidenceIntervalSize, confidenceIntervalType, minimumVariance, preset);
+        regularization(NULL, NULL, NULL, vPC, vZ, vMin, preset);
     }
     /**
-    * Set the regularization parameters for the `Estimator` to the given `preset`. This is a convenience function that simply calls `regularization(NULL, NULL, NULL, -1, "", -1, preset);` */
+    * Set the regularization parameters for the `Estimator` to the given `preset`. This is a convenience function that simply calls `regularization(NULL, NULL, NULL, -1, -1, -1, preset);` */
     void regularization(std::string preset) throw(std::invalid_argument) {
-        regularization(NULL, NULL, NULL, -1, "", -1, preset);
+        regularization(NULL, NULL, NULL, -1, -1, -1, preset);
     }
 #endif
 #endif
-
-    std::string confidenceIntervalType() const { return confidenceIntervalType_; }
-
-    double zConfidenceIntervalSize() const { return z; }
-
-    double minimumVariance() const { return minimumVariance_; }
 
     //</editor-fold>
 
@@ -303,9 +299,9 @@ private:
     int nU_;              ///< the size of the input alphabet
     long N_;            ///< the length of the sample sequence from which the estimates are calculated
 
-    std::string confidenceIntervalType_;
-    double z;
-    double minimumVariance_;
+    double vZ_;
+    double vPC_;
+    double vMin_;
 
     SWIGCODE(%ignore State::operator=;)
     /**
@@ -325,7 +321,7 @@ private:
 
         State& reset(bool estimateVariance = true) {
             pos_.setRoot();
-            f_ = f2_ = 1;
+            f_ = pi_ = f2_ = 1;
             v_ = 0;
             k_ = 0;
             estimateVariance_ = estimateVariance;
@@ -335,6 +331,7 @@ private:
         State& operator <<(const State& state) {
             pos_.set(state.pos_);
             f_ = state.f_;
+            pi_ = state.pi_;
             f2_ = state.f2_;
             v_ = state.v_;
             k_ = state.k_;
@@ -347,48 +344,44 @@ private:
         State& operator <<(std::pair<Symbol, Symbol> ou) {
             k_++;
             /* `n` is the number of possible occurrences for `x_k`, and `c` is the number of occurrences of `x_k`. */
-            if (estimator_.nU_ != 0) { pos_.toSymbol(ou.second); }
-            double n = pos_.count();
+            double n;
+            if (estimator_.nU_ != 0) {
+                n = pos_.count();
+                pos_.toSymbol(ou.second);
+                pi_ *= (pos_.count() + 0.5) / (n + 1.0);
+            }
+            n = pos_.count();
             if (pos_.isSuffix()) { --n; }
             pos_.toSymbol(ou.first);
             double c = pos_.count();
-            double p = (n == 0 ? 1.0 / estimator_.nO_ : c / n);
+            double p = ( n == 0 ? 1.0 / estimator_.nO_ : c / n );
             f_ *= p;
 
             if (not estimateVariance_) { return *this; }
 
-            /* Next we compute a confidence interval for p: */
-            double p_mid = (n == 0 ? 0.5 : c / n);
-            double ci_lower = 0.0, ci_upper = 0.0;
-            if (estimator_.z != 0) {
-                double z2 = estimator_.z * estimator_.z;
-                ci_lower = ci_upper = 1.0;
-                if (n > 0) {
-                    p_mid = (c + 0.5 * z2) / (n + z2);
-                    if (estimator_.confidenceIntervalType_ == "Agresti–Coull") {
-                        ci_lower = ci_upper = estimator_.z * std::sqrt(p_mid * (1 - p_mid) / (n + z2));
-                    } else if (estimator_.confidenceIntervalType_ == "Wilson") {
-                        ci_lower = ci_upper = estimator_.z * std::sqrt(n * p * (1 - p) + z2 / 4) / (n + z2);
-                    } else if (estimator_.confidenceIntervalType_ == "Wilson_CC") { // Wilson score interval with continuity correction (more conservative):
-                        if (c > 0) ci_lower = (0.5 + estimator_.z * std::sqrt(n * p * (1 - p) + z2 / 4 - 0.25 / n + p - 0.5)) / (n + z2);
-                        if (c < n) ci_upper = (0.5 + estimator_.z * std::sqrt(n * p * (1 - p) + z2 / 4 - 0.25 / n - p + 0.5)) / (n + z2);
-                    }
-                }
+            if (estimator_.vPC_ > 0) {
+                c += 0.5 * estimator_.vPC_;
+                n += estimator_.vPC_;
             }
-            double p_lower = std::max(p_mid - ci_lower, 0.0);
-            double p_upper = std::min(p_mid + ci_upper, 1.0);
-            double p_centered = (p_mid < 0.5 ? std::min(p_upper, 0.5) : std::max(p_lower, 0.5));
+
+            /* Use a Wilson confidence interval for p to compute the variance estimate: */
+            double z2 = estimator_.vZ_ * estimator_.vZ_;
+            p = ( n + z2 > 0 ? (c + 0.5 * z2) / (n + z2) : 0.5 ) ;
+            double ci_size = ( z2 > 0 ? std::sqrt(z2 * (n * p * (1 - p) + z2 / 4)) / (n + z2) : 0 );
+            double p_lower = std::max(p - ci_size, 0.0);
+            double p_upper = std::min(p + ci_size, 1.0);
+            double p_centered = (p < 0.5 ? std::min(p_upper, 0.5) : std::max(p_lower, 0.5));
             double p_extreme = std::min(p_lower, 1 - p_upper);
 
             double E_p2_overestimate = p_upper * p_upper;
             double var_p_overestimate = 0.5;
             double var_p_underestimate = 0.0;
-            if (n > 0) {
-                var_p_overestimate = p_centered * (1 - p_centered) / n;
-                var_p_underestimate = p_extreme * (1 - p_extreme) / n;
+            if (n + z2 - 1 >= 1) {
+                var_p_overestimate = p_centered * (1 - p_centered) / (n + z2 - 1);
+                var_p_underestimate = p_extreme * (1 - p_extreme) / (n + z2 - 1);
             }
 
-            v_ = v_ * (E_p2_overestimate - var_p_underestimate) + f2_ * var_p_overestimate;
+            v_ = v_ * std::max(E_p2_overestimate - var_p_underestimate, 0.0) + f2_ * var_p_overestimate;
             f2_ *= E_p2_overestimate;
             return *this;
         }
@@ -403,13 +396,17 @@ private:
         double v() const {
             assert(estimateVariance_);
             double var = v_;
-            if (k_ == 0) { var = 1e-15; }
-            return var;
+            if (k_ == 0) { var = 0; }
+            if (estimator_.vMin_ >= 0.25)
+                return std::min(std::max(var, estimator_.vMin_ / (estimator_.N_ * estimator_.N_ * pi_ * pi_)), 1.0);
+            else
+                return std::min(std::max(var, estimator_.vMin_), 1.0);
         }
 
         const Estimator& estimator_; ///< the `Estimator` that this `State` belongs to
         stree::Position pos_;        ///< the position in the suffix tree for the currently estimated sequence.
         double f_;               ///< the current estimate
+        double pi_;
         double f2_;
         double v_;
         long k_;                 ///< the length of the currently parsed sequence
