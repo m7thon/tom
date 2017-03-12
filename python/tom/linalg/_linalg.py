b8d2140b9548c2fe1fdc6fc656cb6dac78d8cfa1
Michael Thon <m7thon@users.noreply.github.com>
2017-03-12 19:58:32 +0100

    tom.linalg.spectral_norm_expectation now only returns the sampled expectation and no longer the standard deviation.

--- python/tom/linalg/_linalg.py
+++ python/tom/linalg/_linalg.py
@@ -14,13 +14,13 @@ def spectral_norm_expectation(sqrt_V, n = 5):
     """
     Compute the expectation of the spectral norm of a random matrix with independently normally
     distributed entries with zero mean and standard deviation given by `sqrt_V` (i.e., the
-    square root of the element-wise variances) from `n` samples. This returns a tuple of the
-    average spectral norm and the estimated standard deviation.
+    square root of the element-wise variances) from `n` samples.
     """
+    if not sqrt_V.any(): return 0
     spec_norm = np.zeros(n)
     for i in range(n):
-        spec_norm[i] = np.linalg.norm(sqrt_V * np.random.randn(*sqrt_V.shape), ord=2)
-    return np.mean(spec_norm), np.std(spec_norm)
+        spec_norm[i] = np.log(np.linalg.norm(sqrt_V * np.random.randn(*sqrt_V.shape), ord=2))
+    return np.exp(np.mean(spec_norm))
 
 
 def wsvd(sqrt_w_Y, F, sqrt_w_X):
